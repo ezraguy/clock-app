8e21550a272952e751d119aa6c1f6fe9c01fbec6
ezraguy <55058653+ezraguy@users.noreply.github.com>
2020-11-21 18:33:34 +0200

    rewriting the background component

--- src/components/background.jsx
+++ src/components/background.jsx
@@ -5,14 +5,23 @@ import desktopNightTime from '../images/desktop/bg-image-nighttime.jpg';
 import tabletDayTime from '../images/tablet/bg-image-daytime.jpg';
 import tabletNightTime from '../images/tablet/bg-image-nighttime.jpg';
 import mobileDayTime from '../images/mobile/bg-image-daytime.jpg';
-import mobilenightTime from '../images/mobile/bg-image-nighttime.jpg';
+import mobileNightTime from '../images/mobile/bg-image-nighttime.jpg';
 const Background = () => {
     const [backgroundImg, setBackgroundImg] = useState('');
-    const [dayTime, setDayTime] = useState(true);
+    const [dayTime, setDayTime] = useState(false);
     const [screenSize, setScreenSize] = useState(0);
+
     const handleResize = () => {
         let screenWidth = window.innerWidth;
         setScreenSize(screenWidth)
+    }
+    const setBackground = () => {
+        if (screenSize > 1000 && dayTime) return <img src={desktopDayTime} className="backgroundImg" alt="" />;
+        if (screenSize > 1000 && !dayTime) return <img src={desktopNightTime} className="backgroundImg" alt="" />;
+        if (screenSize < 1000 && screenSize > 600 && dayTime) return <img src={tabletDayTime} className="backgroundImg" alt="" />;
+        if (screenSize < 1000 && screenSize > 600 && !dayTime) return <img src={tabletNightTime} className="backgroundImg" alt="" />;
+        if (screenSize <= 600 && dayTime) return <img src={mobileDayTime} className="backgroundImg" alt="" />;
+        if (screenSize <= 600 && !dayTime) return <img src={mobileNightTime} className="backgroundImg" alt="" />;
     }
     useEffect(() => {
         window.addEventListener('resize', handleResize);
@@ -27,24 +36,7 @@ const Background = () => {
         <div className="background">
             <div className="backgroundImg-Wrap">
 
-                {screenSize > 1000 && dayTime &&
-                    <img src={desktopDayTime} className="backgroundImg" alt="" />
-                }
-                {screenSize > 1000 && !dayTime &&
-                    <img src={desktopNightTime} className="backgroundImg" alt="" />
-                }
-                {screenSize < 1000 && screenSize > 600 && dayTime &&
-                    <img src={tabletDayTime} className="backgroundImg" alt="" />
-                }
-                {screenSize < 1000 && screenSize > 600 && !dayTime &&
-                    <img src={tabletDayTime} className="backgroundImg" alt="" />
-                }
-                {screenSize <= 600 && dayTime &&
-                    <img src={mobileDayTime} className="backgroundImg" alt="" />
-                }
-                {screenSize <= 600 && !dayTime &&
-                    <img src={mobileDayTime} className="backgroundImg" alt="" />
-                }
+                {setBackground()}
             </div>
         </div>
     );
