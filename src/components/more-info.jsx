a8d7d9a44c62324c3b09fe821664cd6cc7ff9392
ezraguy <55058653+ezraguy@users.noreply.github.com>
2020-11-27 17:47:49 +0200

    adding all info to information div

--- src/components/more-info.jsx
+++ src/components/more-info.jsx
@@ -14,13 +14,28 @@ const MoreInfo = () => {
         <div className={!moreInfo ? 'more-info ' : 'more-info show-info'}>
             <div className="content-wrap">
                 <div className="content">
-                    <div className="timezone">
-                        <p className="label">Current timezone</p>
-                        <p className="text">{location.time_zone}</p>
+                    <div className="left-panel">
+                        <div className="timezone">
+
+                            <p className="label">Current timezone</p>
+                            <p className="text">{location.time_zone}</p>
+                        </div>
+                        <div className="day-of-year">
+                            <p className="label">day of the year</p>
+                            <p className="text">{time.day_of_year}</p>
+                        </div>
                     </div>
-                    <div className="day-of-year">
-                        <p className="label">day of the year</p>
-                        <p className="text">{time.day_of_year}</p>
+                    <div className="divider"></div>
+                    <div className="right-panel">
+
+                        <div className="day-of-week">
+                            <p className="label">day of the week</p>
+                            <p className="text">{time.day_of_week}</p>
+                        </div>
+                        <div className="week-number">
+                            <p className="label">week number</p>
+                            <p className="text">{time.week_number}</p>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/context/more-info-context.js
+++ src/context/more-info-context.js
@@ -3,7 +3,7 @@ import React, { createContext, useState } from 'react';
 export const MoreInfoContext = createContext();
 
 export const MoreInfoProvider = props => {
-    const [moreInfo, setMoreInfo] = useState(true)
+    const [moreInfo, setMoreInfo] = useState(false)
     return (
         <MoreInfoContext.Provider value={[moreInfo, setMoreInfo]}>
             {props.children}
