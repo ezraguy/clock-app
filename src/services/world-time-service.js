a7ecaae371423d917ed33918c7146bb3c3ae57c1
ezraguy <55058653+ezraguy@users.noreply.github.com>
2020-11-28 03:34:27 +0200

    removing request header

--- src/services/world-time-service.js
+++ src/services/world-time-service.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 const getTime = async (ip) => {
 
     const url = `https://worldtimeapi.org/api/ip/${ip}`
-    const headers = { 'Access-Control-Allow-Origin': '*' };
+
     try {
-        const data = await axios(url, {}, { headers });
+        const data = await axios(url);
         return data.data;
 
     } catch (err) {
