b69e34eb5404dc90cc4388e0d4b091b1cf809231
ezraguy <55058653+ezraguy@users.noreply.github.com>
2020-11-28 03:11:19 +0200

    fixing api call

--- src/services/world-time-service.js
+++ src/services/world-time-service.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 const getTime = async (ip) => {
 
-    const url = `http://worldtimeapi.org/api/ip/${ip}`
+    const url = `https://worldtimeapi.org/api/ip/${ip}`
     const headers = { 'Access-Control-Allow-Origin': '*' };
     try {
         const data = await axios(url, {}, { headers });
